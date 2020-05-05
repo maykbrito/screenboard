8e51ca5bc83a9706e286b247cbc22028daede2fe
Mayk Brito <maykbrito@gmail.com>
2020-05-04 20:44:14 -0400

    :sparkles: electron shortcuts

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-const { app, BrowserWindow, screen } = require('electron')
+const { app, BrowserWindow, screen, globalShortcut } = require('electron')
 
-
+let win = null
 
 function createWindow () {
     const mainScreen = screen.getPrimaryDisplay();
     const dimensions = mainScreen.size;
 
   // Create the browser window.
-  const win = new BrowserWindow({
+  win = new BrowserWindow({
     width: dimensions.width,
     height: dimensions.height,
     transparent:true,
@@ -23,10 +23,16 @@ function createWindow () {
   win.loadFile('index.html')
 }
 
+
+function createShortcuts() {
+    globalShortcut.register('Cmd+F12', recreateWindow)
+}
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.whenReady().then(createWindow)
+app.whenReady()
+.then(createWindow)
+.then(createShortcuts)
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
@@ -37,13 +43,16 @@ app.on('window-all-closed', () => {
   }
 })
 
-app.on('activate', () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow()
-  }
-})
+app.on('activate', recreateWindow)
+
+
+function recreateWindow() {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow()
+    }
+}
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
