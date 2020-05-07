90b9399a9f4e9261478daff43aec2a910c11daca
Almir Jakubaitis <almir@icone3.com.br>
2020-05-07 00:07:57 -0300

    simple solution

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ function createShortcuts() {
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady()
-.then(setTimeout(createWindow, 500)) // set timeout for linux compability
+.then(createWindow)
 .then(createShortcuts)
 
 // Quit when all windows are closed.
@@ -52,17 +52,10 @@ function recreateWindow() {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) {
-      setTimeout(createWindow, 500) // set timeout for linux compability
-        
-    } else {
-      win.loadFile('index.html') // else to force windows compability
-    }
-
-    
-    
-    
-    
-
+        createWindow()
+      } else {
+        win.loadFile('index.html') // else to force windows compability
+      }
 }
 
 // In this file you can include the rest of your app's specific main process
