9deb690a7c65b3e57e5a9c84d5276ce5e9639476
cl4udino <lhcs10@gmail.com>
2020-05-07 15:59:26 -0300

    Changes to work on Linux

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ function createShortcuts() {
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady()
-.then(createWindow)
+.then(setTimeout(createWindow, 200))
 .then(createShortcuts)
 
 // Quit when all windows are closed.
@@ -52,7 +52,7 @@ function recreateWindow() {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) {
-        createWindow()
+      setTimeout(createWindow, 200)
     }
 }
 
