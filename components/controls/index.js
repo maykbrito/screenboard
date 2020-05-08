ab61f657e627978ebe125db6ee6efe7939a6ef49
dioneijd <49819948+dioneijd@users.noreply.github.com>
2020-05-08 11:00:03 -0300

    Revert "feat: top dropdown menu"

--- components/controls/index.js
+++ components/controls/index.js
@@ -1,13 +1,13 @@
 import Size from './Size.js'
 import Color from './Color.js'
-import Tools from './Tools.js'
+import Eraser from './Eraser.js'
 
 export default function(context) {
 
     const controls = [
         Size(context),
-        Color(context),
-        Tools(context)
+        Eraser(context),
+        Color(context)
     ]
 
     function updateAll() {
