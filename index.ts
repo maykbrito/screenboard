aab8691c6b9a0c640c03c67a55b01e015e266663
Mayk Brito <maykbrito@gmail.com>
2021-08-21 23:40:31 -0300

    add typescript

--- index.ts
+++ index.ts
@@ -1,11 +1,11 @@
-const { app, BrowserWindow, screen, globalShortcut } = require('electron')
+import { app, BrowserWindow, screen, globalShortcut } from 'electron'
 
-let win = null
+let win: BrowserWindow
 app.allowRendererProcessReuse = true
 
-function createWindow () {
-    const mainScreen = screen.getPrimaryDisplay();
-    const dimensions = mainScreen.size;
+function createWindow() {
+  const mainScreen = screen.getPrimaryDisplay()
+  const dimensions = mainScreen.size
 
   // Create the browser window.
   win = new BrowserWindow({
@@ -14,6 +14,7 @@ function createWindow () {
     transparent: true,
     frame: false,
     titleBarStyle: 'customButtonsOnHover',
+    alwaysOnTop: true,
     webPreferences: {
       nodeIntegration: true
     }
@@ -25,21 +26,19 @@ function createWindow () {
   // win.webContents.toggleDevTools()
 }
 
-
 function createShortcuts() {
-  const { 
-    reopen = 'Alt+Shift+w',
-  } = require('./src/shortcuts')
-  
+  const { reopen = 'Alt+Shift+w' } = require('../src/shortcuts')
+
   globalShortcut.register(reopen, WindowVisibility.toggle)
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.whenReady()
-.then(setTimeout(createWindow, 200))
-.then(createShortcuts)
+app
+  .whenReady()
+  .then(() => setTimeout(createWindow, 200))
+  .then(createShortcuts)
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
@@ -52,30 +51,28 @@ app.on('window-all-closed', () => {
 
 app.on('activate', recreateWindow)
 
-
 function recreateWindow() {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) {
-      setTimeout(createWindow, 200)
-    }
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) {
+    setTimeout(createWindow, 200)
+  }
 }
 
-
-/** 
- *  
+/**
+ *
  *  Toggle Window Visibility
  *  in macOS we can use win.show() or win.hide() to toggle visibility.
- * 
+ *
  *  in Win and Linux we can use win.minimize() or win.maximize() to toggle visibility.
  */
 const isMacOS = process.platform === 'darwin'
 
 const WindowVisibility = {
   isVisible: true,
-  
+
   toggle() {
-    const show = isMacOS ? () => recreateWindow() : () =>  win.maximize()
+    const show = isMacOS ? () => recreateWindow() : () => win.maximize()
     const hide = isMacOS ? () => win.close() : () => win.minimize()
 
     this.isVisible ? show() : hide()
@@ -83,5 +80,7 @@ const WindowVisibility = {
   }
 }
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+// Allows app display on top of fullscreen apps
+if(isMacOS) {
+  app.dock.hide()
+}
